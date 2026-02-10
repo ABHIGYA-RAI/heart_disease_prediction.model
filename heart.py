f6a9fb039d5d87630c492f1acdb2a95d73efac08
Abhigya Rai <023bscit049@sxc.edu.np>
2026-02-10 23:25:26 +0545

    LRM will predict the probability of heart disease

--- heart.py
+++ heart.py
@@ -2,10 +2,11 @@ import streamlit as slt
 import numpy as np
 import pickle
 
+
 slt.title('Heart Disease Prediction Machine Learning Model')
 user_input = slt.text_area("Enter the heart data configurations")
 if slt.button('Predict if you have heart disease or not'):
-    with open('lrm.pkl', 'rb') as file:
+    with open('LRM.pkl', 'rb') as file:
         model = pickle.load(file)
     input_2d = np.array([user_input.split(',')],dtype=float)
     prediction = model.predict(input_2d)
